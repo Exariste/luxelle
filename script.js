5a243d5a8ddff6645a40a008fe88986f936a8143
Exariste <zaman.msc.med@gmail.com>
2026-06-19 02:08:13 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// smooth scroll effect
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+a.addEventListener("click", function(e){
+e.preventDefault();
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior:"smooth"
+});
+});
+});
