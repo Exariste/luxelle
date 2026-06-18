b35f3c0b8c703274cca1d72c57c0d17865079108
Exariste <zaman.msc.med@gmail.com>
2026-06-18 22:43:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-// লোগো রিলিজ বা স্মুথ অ্যানিমেশনের জন্য বেসিক সেটআপ
-console.log("LUXELLE & FAIR: Luxury Experience Initialized");
+document.querySelector('.logo img').addEventListener('mouseover', () => {
+    console.log("Welcome to Luxelle & Fair");
+});
